14a7b8fe574c86fc50f11c636266330eacf97935
Zack Stout <zackstout@gmail.com>
2018-01-31 19:16:03 -0600

    got basic grid working in p5

--- sketch.js
+++ sketch.js
@@ -1,4 +1,35 @@
 
+var snakeCells = [];
+var snake = [{r: 0, c: 4}, {r: 1, c: 4}, {r: 2, c: 4}, {r: 3, c: 4}];
+var tail = {r: 1, c: 1};
+var direction = 'd';
+var w = 600;
+var h = 600;
+var cellSize = 30;
+var numCells = w / cellSize;
+
+function setup() {
+  createCanvas(w, h);
+  drawGrid();
+
+}
+
+function draw() {
+
+}
+
+function drawGrid() {
+  fill(100);
+  // var rect1 = rect(0, 0, 100, 100);
+  for (var i=0; i < numCells; i++) {
+    for (var j=0; j < numCells; j++) {
+      var cell = rect(i * cellSize, j * cellSize, cellSize, cellSize);
+      snakeCells.push(cell);
+    }
+  }
+
+
+}
 
   var allSnakeCells = [];
   var snake = [{r: 0, c: 4}, {r: 1, c: 4}, {r: 2, c: 4}, {r: 3, c: 4}];
@@ -124,16 +155,16 @@
     }
     // console.log(direction);
   };
-
-  snakeGrid(15);
-
-  var apple = {r: 10, c: 1};
-  var appleCell = 'col' + apple.c + 'row' + apple.r;
-  document.getElementById(appleCell).className = 'apple';
-
-  changeSnake();
-
-  var snakeInt;
-  vm.startSnake = function() {
-    snakeInt = setInterval(changeSnake, 230);
-  };
+  //
+  // snakeGrid(15);
+  //
+  // var apple = {r: 10, c: 1};
+  // var appleCell = 'col' + apple.c + 'row' + apple.r;
+  // document.getElementById(appleCell).className = 'apple';
+  //
+  // changeSnake();
+  //
+  // var snakeInt;
+  // vm.startSnake = function() {
+  //   snakeInt = setInterval(changeSnake, 230);
+  // };
